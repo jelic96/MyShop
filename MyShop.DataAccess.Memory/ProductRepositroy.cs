29583131321f593e168934ff05b72740b8a665ac
Marko <Marko@DESKTOP-C6DTE0A>
2023-02-27 23:51:15 +0100

    Product Model and InMemory

---
+++ MyShop.DataAccess.Memory/ProductRepositroy.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using MyShop.Core.Models;
+
+namespace MyShop.DataAccess.Memory
+{
+    public class ProductRepositroy
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Product> products = new List<Product>();
+
+        public ProductRepositroy()
+        {
+            products = cache["products"] as List<Product>;
+            if (products == null)
+            {
+                products = new List<Product>();
+            }
+        }
+
+            public void Commit()
+            {
+                cache["products"] = products;
+            }
+
+            public void Insert(Product p)
+            {
+                products.Add(p);
+            }
+
+            public void Update(Product product)
+            {
+                Product productToUpdate = products.Find(p => p.ID == product.ID);
+
+                if (productToUpdate != null)
+                {
+                    productToUpdate = product;
+                }
+                else
+                {
+                    throw new Exception("Product not found");
+                }
+
+
+            }
+
+            public Product Find(string ID)
+            {
+                Product productToFind = products.Find(p => p.ID == ID);
+
+                if (productToFind != null)
+                {
+                    return productToFind;
+                }
+                else
+                {
+                    throw new Exception("Product not found");
+                }
+            }
+
+            public IQueryable<Product> Collection()
+            {
+                return products.AsQueryable();
+            }
+
+            public void Delete(string ID)
+            {
+                Product productToDelete = products.Find(p => p.ID == ID);
+
+                if (productToDelete != null)
+                {
+                    products.Remove(productToDelete);
+                }
+                else
+                {
+                    throw new Exception("Product not found");
+                }
+            }
+
+     }
+}
